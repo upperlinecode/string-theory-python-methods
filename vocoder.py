46a2d2e580ca7d064b8c91d2595d405aafdf659c
Aaron <afink.2020@saintannsny.org>
2018-12-16 21:18:41 -0500

    replace method with function

--- vocoder.py
+++ vocoder.py
@@ -1,53 +1,53 @@
-# 1. Write a method called shout that takes in a string as an argument and returns it in all caps.
+# 1. Write a function called shout that takes in a string as an argument and returns it in all caps.
 
 
 
 
-# 2. Write a method called whisper that takes in a string as an argument and returns it in all lowercase.
-
-
-
-
-
-# 3. Write a method called how_many_letters that takes in a string as an argument and returns the number of letters in that string.
+# 2. Write a function called whisper that takes in a string as an argument and returns it in all lowercase.
 
 
 
 
 
-# 4. Write a method called work_it that takes in a string as an argument and returns it backwards and with the first letter capitalized.
+# 3. Write a function called how_many_letters that takes in a string as an argument and returns the number of letters in that string.
+
+
+
+
+
+# 4. Write a function called work_it that takes in a string as an argument and returns it backwards and with the first letter capitalized.
 #    For example, work_it("I put my thing down flip it and reverse it") would return "Ti esrever dna ti pilf nwod gniht ym tup i"
 
 
 
 
 
-# 5. Write a method called repeat_it that takes in a word and a number, and returns the word that many times.
+# 5. Write a function called repeat_it that takes in a word and a number, and returns the word that many times.
 #    For example, repeat_it("Banana", 3) would return "BananaBananaBanana"
 
 
 
 
 
-# 6. Write a method called will_it_nametag that takes in a name and a number, and tells you whether the name can be printed on a nametag that size.
+# 6. Write a function called will_it_nametag that takes in a name and a number, and tells you whether the name can be printed on a nametag that size.
 #    For example, will_it_nametag("Peter", 10) will return True, but will_it_nametag("Peter", 4) will return False, because "Peter" is 5 letters long, and needs at least 5 spaces to fit on a nametag.
 
 
 
 
 
-# 7. Write a method called add_liar that takes in two numbers and prints out a lie like "Sorry, I don't know how to add ___ and ___."
-#    BUT even though it *prints* that it doesn't know the answer, have the method return the correct answer anyways.
+# 7. Write a function called add_liar that takes in two numbers and prints out a lie like "Sorry, I don't know how to add ___ and ___."
+#    BUT even though it *prints* that it doesn't know the answer, have the function return the correct answer anyways.
 
 
 
 
 
-# 8. CHALLENGE: Now that you've written all these methods, try combining them. what would happen if you tried to call repeat_it(work_it("Stressed"), 3) ?
+# 8. CHALLENGE: Now that you've written all these functions, try combining them. what would happen if you tried to call repeat_it(work_it("Stressed"), 3) ?
 
 
 
 
 
-# 9. MEGA CHALLENGE: Write a method called doubler that takes in a string and returns it with every letter doubled.
+# 9. MEGA CHALLENGE: Write a function called doubler that takes in a string and returns it with every letter doubled.
 #    For example, doubler("Lost in the Woods") would return "LLoosstt iinn tthhee WWooooddss"
